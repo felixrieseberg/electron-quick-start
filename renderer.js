d3d3e3484758f06497d8a7ac20c9588288e57ab4
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-22 16:24:06 -0700

    :wrench: Passed env

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { remote } = require('electron')
+
+document.querySelector('#launch').onclick = () => {
+  process.env.HI_FROM_ELECTRON = 'Hello!';
+  remote.shell.openExternal('https://google.com');
+}
