cf54b4283b1f2d468df083cf9526aa9de5f107b5
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-29 17:50:55 -0700

    :art: Basic research

--- renderer.js
+++ renderer.js
@@ -1,3 +1,34 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const ss = 'https://a.slack-edge.com/c00d19/img/emoji_2017_12_06/sheet_apple_64_indexed_256.png'
+
+function getDiv() {
+  const div = new HTMLDivElement()
+}
+
+$('#clear').click(() => $('#testarea').empty())
+
+$('#img-simple').click(() => {
+  for (let i = 0; i < 50; i++) {
+    $('#testarea').append(`<img src="${ss}" />`)
+  }
+})
+
+$('#img-tick').click(() => {
+  for (let i = 0; i < 50; i++) {
+    process.nextTick(() => $('#testarea').append(`<img src="${ss}" />`))
+  }
+})
+
+$('#img-div').click(() => {
+  const pos = `background-position:${Math.random()}% ${Math.random()};background-size:5200% 5200%;`
+  const size = `height:50px;width:50px;`
+  const style = `background-image: url(${ss});${pos};${size}`
+
+  for (let i = 0; i < 50; i++) {
+    $('#testarea').append(`<div style="${style}"></div>`)
+  }
+})
+
